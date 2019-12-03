85a6431bc90141cb8909da2b3566f3ab556135f5
SiriusZ-BOTTLE <582988860@qq.com>
2019-12-03 22:45:02 +0800

    歌单接口实现

--- MucMucServer/src/main/java/org/mucmuc/main/web/Controller_SongList.java
+++ MucMucServer/src/main/java/org/mucmuc/main/web/Controller_SongList.java
@@ -1,8 +1,14 @@
 package org.mucmuc.main.web;
 
+import org.mucmuc.main.entity.InteractionEntity.ResultEntity;
+import org.mucmuc.main.entity.SongList;
+import org.mucmuc.main.service.implement.Service_SongList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 //控制器注解
 @ResponseBody
@@ -11,7 +17,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/songList")
 public class Controller_SongList {
 
+    @Resource
+    private Service_SongList songListService;
 
+    /**
+     * 更新歌单信息
+     * @param songList
+     * @return
+     */
+    @RequestMapping(value = "/update",method= RequestMethod.POST,produces ="application/json;charset=UTF-8")
+    public ResultEntity update(SongList songList)
+    {
+        return songListService.update(songList);
+    }
+
+
+    /**
+     * 删除歌单
+     * @param songList
+     * @return
+     */
+    @RequestMapping(value = "/update",method= RequestMethod.POST,produces ="application/json;charset=UTF-8")
+    public ResultEntity delete(SongList songList)
+    {
+        return songListService.delete(songList);
+    }
 
 
 }
