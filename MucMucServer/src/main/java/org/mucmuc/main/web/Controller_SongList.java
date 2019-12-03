b360f197e2b9bf8bb7f7168aa5e2a9b4e166ca92
SiriusZ-BOTTLE <582988860@qq.com>
2019-12-03 21:48:03 +0800

    Create Controller_SongList.java
    歌单接口

---
+++ MucMucServer/src/main/java/org/mucmuc/main/web/Controller_SongList.java
@@ -0,0 +1,17 @@
+package org.mucmuc.main.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//控制器注解
+@ResponseBody
+@Controller
+//@RestController//等价注解
+@RequestMapping(value = "/songList")
+public class Controller_SongList {
+
+
+
+
+}
