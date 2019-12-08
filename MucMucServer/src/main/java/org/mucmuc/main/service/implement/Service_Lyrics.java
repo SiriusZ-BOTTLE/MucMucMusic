bfcbb9e780fa80eee430e6c0e6606e8101aaddf8
jbasjkbc <47034739+jbasjkbc@users.noreply.github.com>
2019-12-08 18:57:56 +0800

    add try-catch

--- MucMucServer/src/main/java/org/mucmuc/main/service/implement/Service_Lyrics.java
+++ MucMucServer/src/main/java/org/mucmuc/main/service/implement/Service_Lyrics.java
@@ -23,103 +23,139 @@ public class Service_Lyrics implements Interface_Lyrics_service {
     @Override
     public ResultEntity get(Lyrics lyrics) {
 
-        Boolean success = Boolean.FALSE;
-        String errorMsg = "";//错误信息默认为空
-        String opMsg="done";//操作信息默认为完成
-        if (lyrics == null){
-            errorMsg = "请求数据不能为空！";
-        } else {
-            //根据请求参数查询用户信息
-            lyrics = dao_Lyrics.queryByID_Song(lyrics);
+        try{
+            Boolean success = Boolean.FALSE;
+            String errorMsg = "";//错误信息默认为空
+            String opMsg="done";//操作信息默认为完成
             if (lyrics == null){
-                errorMsg = "歌词信息不存在！";
-            }else if(lyrics.getFlag_Pure_Lyrics() == Boolean.TRUE){
-                errorMsg = "为纯音乐，歌词不存在！";
-            }else if(lyrics.getContent_Lyrics() == null || lyrics.getContent_Lyrics().equals("")){
-                errorMsg = "歌词为空！";
-            }else {
-                success = Boolean.TRUE;
+                errorMsg = "请求数据不能为空！";
+            } else {
+                //根据请求参数查询用户信息
+                lyrics = dao_Lyrics.queryByID_Song(lyrics);
+                if (lyrics == null){
+                    errorMsg = "歌词信息不存在！";
+                }else if(lyrics.getFlag_Pure_Lyrics() == Boolean.TRUE){
+                    errorMsg = "为纯音乐，歌词不存在！";
+                }else if(lyrics.getContent_Lyrics() == null || lyrics.getContent_Lyrics().equals("")){
+                    errorMsg = "歌词为空！";
+                }else {
+                    success = Boolean.TRUE;
+                }
             }
+
+            //封装返回结果
+            ResultEntity resultEntity = new ResultEntity(success,errorMsg, opMsg, lyrics);
+            return resultEntity;
+        }catch (Exception e){
+            ResultEntity resultEntity = new ResultEntity();
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("获得歌词信息失败！");
+            return resultEntity;
         }
 
-        //封装返回结果
-        ResultEntity resultEntity = new ResultEntity(success,errorMsg, opMsg, lyrics);
-        return resultEntity;
     }
 
     @Override
     public ResultEntity update(Lyrics lyrics) {
 
-        Boolean success = Boolean.FALSE;
-        String errorMsg = "";//错误信息默认为空
-        String opMsg="done";//操作信息默认为完成
-        if (lyrics == null){
-            errorMsg = "请求数据不能为空！";
-        } else if (lyrics.getId_Lyrics() == null || lyrics.getId_Lyrics().equals("")){
-            errorMsg = "歌词编号不能为空！";
-        }else {
-            //更新用户信息
-            int resultRow = dao_Lyrics.update(lyrics);
-            if (resultRow < 1){
-                opMsg = "更新歌词失败！";
+        try{
+            Boolean success = Boolean.FALSE;
+            String errorMsg = "";//错误信息默认为空
+            String opMsg="done";//操作信息默认为完成
+            if (lyrics == null){
+                errorMsg = "请求数据不能为空！";
+            } else if (lyrics.getId_Lyrics() == null || lyrics.getId_Lyrics().equals("")){
+                errorMsg = "歌词编号不能为空！";
             }else {
-                success = Boolean.TRUE;
+                //更新用户信息
+                int resultRow = dao_Lyrics.update(lyrics);
+                if (resultRow < 1){
+                    opMsg = "更新歌词失败！";
+                }else {
+                    success = Boolean.TRUE;
+                }
             }
+
+            //封装返回结果
+            ResultEntity resultEntity = new ResultEntity(success, errorMsg,opMsg , null);
+            return resultEntity;
+        }catch (Exception e){
+            ResultEntity resultEntity = new ResultEntity();
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("更新歌词失败！");
+            return resultEntity;
         }
 
-        //封装返回结果
-        ResultEntity resultEntity = new ResultEntity(success, errorMsg,opMsg , null);
-        return resultEntity;
     }
 
     @Override
     public ResultEntity delete(Lyrics lyrics) {
 
-        Boolean success = Boolean.FALSE;
-        String errorMsg = "";//错误信息默认为空
-        String opMsg="done";//操作信息默认为完成
-        if (lyrics == null){
-            errorMsg = "请求数据不能为空！";
-        } else if (lyrics.getId_Song() == null){
-            errorMsg = "歌曲编号不能为空！";
-        } else {
-            //删除歌词信息
-            int resultRow = dao_Lyrics.deleteBySong(lyrics);
-            if (resultRow < 1){
-                opMsg = "删除歌词失败！";
-            }else {
-                success = Boolean.TRUE;
+        try{
+            Boolean success = Boolean.FALSE;
+            String errorMsg = "";//错误信息默认为空
+            String opMsg="done";//操作信息默认为完成
+            if (lyrics == null){
+                errorMsg = "请求数据不能为空！";
+            } else if (lyrics.getId_Song() == null){
+                errorMsg = "歌曲编号不能为空！";
+            } else {
+                //删除歌词信息
+                int resultRow = dao_Lyrics.deleteBySong(lyrics);
+                if (resultRow < 1){
+                    opMsg = "删除歌词失败！";
+                }else {
+                    success = Boolean.TRUE;
+                }
             }
+
+            //封装返回结果
+            ResultEntity resultEntity = new ResultEntity(success, errorMsg,opMsg , null);
+            return resultEntity;
+        }catch (Exception e){
+            ResultEntity resultEntity = new ResultEntity();
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("删除歌词失败！");
+            return resultEntity;
         }
 
-        //封装返回结果
-        ResultEntity resultEntity = new ResultEntity(success, errorMsg,opMsg , null);
-        return resultEntity;
     }
 
     @Override
     public ResultEntity add(Lyrics lyrics) {
 
-        Boolean success = Boolean.FALSE;
-        String errorMsg = "";//错误信息默认为空
-        String opMsg="done";//操作信息默认为完成
-        if (lyrics == null){
-            errorMsg = "请求数据不能为空！";
-        } else if (lyrics.getFlag_Pure_Lyrics() == Boolean.TRUE){
-            errorMsg = "为纯音乐，不需要歌词！";
-        } else {
+        try{
+            Boolean success = Boolean.FALSE;
+            String errorMsg = "";//错误信息默认为空
+            String opMsg="done";//操作信息默认为完成
+            if (lyrics == null){
+                errorMsg = "请求数据不能为空！";
+            } else if (lyrics.getFlag_Pure_Lyrics() == Boolean.TRUE){
+                errorMsg = "为纯音乐，不需要歌词！";
+            } else {
 
-            //插入用户信息
-            int resultRow = dao_Lyrics.insertNew(lyrics);
-            if (resultRow < 1){
-                opMsg = "添加歌词失败！";
-            }else {
-                success = Boolean.TRUE;
+                //插入用户信息
+                int resultRow = dao_Lyrics.insertNew(lyrics);
+                if (resultRow < 1){
+                    opMsg = "添加歌词失败！";
+                }else {
+                    success = Boolean.TRUE;
+                }
             }
+
+            //封装返回结果
+            ResultEntity resultEntity = new ResultEntity(success, errorMsg,opMsg , null);
+            return resultEntity;
+        }catch (Exception e){
+            ResultEntity resultEntity = new ResultEntity();
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("添加歌词信息！");
+            return resultEntity;
         }
 
-        //封装返回结果
-        ResultEntity resultEntity = new ResultEntity(success, errorMsg,opMsg , null);
-        return resultEntity;
     }
 }
--- MucMucServer/src/main/java/org/mucmuc/main/service/implement/Service_SongList.java
+++ MucMucServer/src/main/java/org/mucmuc/main/service/implement/Service_SongList.java
@@ -25,40 +25,56 @@ public class Service_SongList implements Interface_SongList_service {
     public ResultEntity getRandom(Integer num) {
         ResultEntity resultEntity =new ResultEntity();
 
-        if(num<=0||num>100)
-        {
-            resultEntity.setInfo_error("<ERROR> num must be in range[1,100]");
+        try {
+            if(num<=0||num>100)
+            {
+                resultEntity.setInfo_error("<ERROR> num must be in range[1,100]");
+                return resultEntity;
+            }
+
+            resultEntity.setObject(songListDao.queryRandom(num));
+            resultEntity.setState(true);
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("随机获得歌单信息失败！");
             return resultEntity;
         }
 
-        resultEntity.setObject(songListDao.queryRandom(num));
-        resultEntity.setState(true);
-        return resultEntity;
     }
 
     @Override
     public ResultEntity get(SongList songList) {
         ResultEntity resultEntity=new ResultEntity();
 
-        if(songList.getId_SL()==null)
-        {
-            resultEntity.setInfo_error("<ERROR> id_SL is NULL");
+        try{
+            if(songList.getId_SL()==null)
+            {
+                resultEntity.setInfo_error("<ERROR> id_SL is NULL");
+                return resultEntity;
+            }
+
+            //数据库中获取
+            SongList songList_db=songListDao.queryByPK(songList);
+
+            if(songList_db==null)
+            {
+                resultEntity.setInfo_operation("歌单不存在");
+                return resultEntity;
+            }
+
+            resultEntity.setState(true);
+            resultEntity.setObject(songList_db);
+
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("获取歌单信息失败！");
             return resultEntity;
         }
 
-        //数据库中获取
-        SongList songList_db=songListDao.queryByPK(songList);
-
-        if(songList_db==null)
-        {
-            resultEntity.setInfo_operation("歌单不存在");
-            return resultEntity;
-        }
-
-        resultEntity.setState(true);
-        resultEntity.setObject(songList_db);
-
-        return resultEntity;
     }
 
     @Override
@@ -66,141 +82,190 @@ public class Service_SongList implements Interface_SongList_service {
 
         ResultEntity resultEntity=new ResultEntity();
 
-        if(songList.getName_SL()==null)
-        {
-            resultEntity.setInfo_error("<ERROR> name_SL is NULL");
+        try{
+            if(songList.getName_SL()==null)
+            {
+                resultEntity.setInfo_error("<ERROR> name_SL is NULL");
+                return resultEntity;
+            }
+
+            List<SongList> list=songListDao.queryByName(songList);
+
+            resultEntity.setObject(list);
+            resultEntity.setState(true);
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("根据歌名获取歌单信息失败！");
             return resultEntity;
         }
 
-        List<SongList> list=songListDao.queryByName(songList);
-
-        resultEntity.setObject(list);
-        resultEntity.setState(true);
-        return resultEntity;
     }
 
     @Override
     public ResultEntity getUserSongList(User user) {
         ResultEntity resultEntity=new ResultEntity();
 
-        if(user.getId_User()==null)
-        {
-            resultEntity.setInfo_error("<ERROR> id_User is NULL");
+        try{
+            if(user.getId_User()==null)
+            {
+                resultEntity.setInfo_error("<ERROR> id_User is NULL");
+                return resultEntity;
+            }
+            resultEntity.setObject(songListDao.queryByUserID(user.getId_User()));
+            resultEntity.setState(true);
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("获取用户创建的歌单信息失败！");
             return resultEntity;
         }
-        resultEntity.setObject(songListDao.queryByUserID(user.getId_User()));
-        resultEntity.setState(true);
-        return resultEntity;
+
     }
 
     @Override
     public ResultEntity addSongToSongList(Song song, SongList songList) {
         ResultEntity resultEntity=new ResultEntity();
 
-        if(song.getId_Song()==null||songList.getId_SL()==null)
-        {
-            resultEntity.setInfo_error("<ERROR> id_Song or id_SL is NULL");
+        try{
+            if(song.getId_Song()==null||songList.getId_SL()==null)
+            {
+                resultEntity.setInfo_error("<ERROR> id_Song or id_SL is NULL");
+                return resultEntity;
+            }
+
+            Map_SL_S map_SL_S=new Map_SL_S();
+            map_SL_S.setId_Song(song.getId_Song());
+            map_SL_S.setId_SL(songList.getId_SL());
+
+            //从数据库中获取
+            Map_SL_S map_SL_S_db=map_S_SL_DAO.queryByPK(map_SL_S);
+
+            if(map_SL_S_db!=null)
+            {
+                resultEntity.setInfo_operation("歌单中已经包含该歌曲");
+                return resultEntity;
+            }
+            //插入到数据库
+            map_S_SL_DAO.insertNew(map_SL_S);
+            resultEntity.setState(true);
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("往歌单中添加歌曲失败！");
             return resultEntity;
         }
 
-        Map_SL_S map_SL_S=new Map_SL_S();
-        map_SL_S.setId_Song(song.getId_Song());
-        map_SL_S.setId_SL(songList.getId_SL());
-
-        //从数据库中获取
-        Map_SL_S map_SL_S_db=map_S_SL_DAO.queryByPK(map_SL_S);
-
-        if(map_SL_S_db!=null)
-        {
-            resultEntity.setInfo_operation("歌单中已经包含该歌曲");
-            return resultEntity;
-        }
-        //插入到数据库
-        map_S_SL_DAO.insertNew(map_SL_S);
-        resultEntity.setState(true);
-        return resultEntity;
     }
 
     @Override
     public ResultEntity removeSongFromSongList(Song song, SongList songList) {
         ResultEntity resultEntity=new ResultEntity();
 
-        if(song.getId_Song()==null||songList.getId_SL()==null)
-        {
-            resultEntity.setInfo_error("<ERROR> id_Song or id_SL is NULL");
+        try{
+            if(song.getId_Song()==null||songList.getId_SL()==null)
+            {
+                resultEntity.setInfo_error("<ERROR> id_Song or id_SL is NULL");
+                return resultEntity;
+            }
+
+            Map_SL_S map_SL_S=new Map_SL_S();
+            map_SL_S.setId_Song(song.getId_Song());
+            map_SL_S.setId_SL(songList.getId_SL());
+
+            //从数据库中获取
+            Map_SL_S map_SL_S_db=map_S_SL_DAO.queryByPK(map_SL_S);
+
+            if(map_SL_S_db==null)
+            {
+                resultEntity.setInfo_operation("歌单中不存在该歌曲");
+                return resultEntity;
+            }
+            //从数据库中删除
+            map_S_SL_DAO.deleteByPK(map_SL_S);
+            resultEntity.setState(true);
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("从歌单中移走歌曲失败！");
             return resultEntity;
         }
 
-        Map_SL_S map_SL_S=new Map_SL_S();
-        map_SL_S.setId_Song(song.getId_Song());
-        map_SL_S.setId_SL(songList.getId_SL());
-
-        //从数据库中获取
-        Map_SL_S map_SL_S_db=map_S_SL_DAO.queryByPK(map_SL_S);
-
-        if(map_SL_S_db==null)
-        {
-            resultEntity.setInfo_operation("歌单中不存在该歌曲");
-            return resultEntity;
-        }
-        //从数据库中删除
-        map_S_SL_DAO.deleteByPK(map_SL_S);
-        resultEntity.setState(true);
-        return resultEntity;
     }
 
     @Override
     public ResultEntity update(SongList songList) {
 
         ResultEntity resultEntity=new ResultEntity();
-        if(songList.getId_SL()==null)
-        {
-            resultEntity.setInfo_error("<ERROR> id_SL is NULL");
+
+        try{
+            if(songList.getId_SL()==null)
+            {
+                resultEntity.setInfo_error("<ERROR> id_SL is NULL");
+                return resultEntity;
+            }
+
+            //数据库中获取
+            SongList songList_db=songListDao.queryByPK(songList);
+
+            if(songList_db==null)
+            {
+                resultEntity.setInfo_operation("歌单不存在");
+                return resultEntity;
+            }
+
+            songListDao.update(songList);
+            //再次获取一遍
+            songList_db=songListDao.queryByPK(songList);
+
+            resultEntity.setObject(songList_db);
+            resultEntity.setState(true);
+
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("更新歌单信息失败");
             return resultEntity;
         }
 
-        //数据库中获取
-        SongList songList_db=songListDao.queryByPK(songList);
-
-        if(songList_db==null)
-        {
-            resultEntity.setInfo_operation("歌单不存在");
-            return resultEntity;
-        }
-
-        songListDao.update(songList);
-        //再次获取一遍
-        songList_db=songListDao.queryByPK(songList);
-
-        resultEntity.setObject(songList_db);
-        resultEntity.setState(true);
-
-        return resultEntity;
     }
 
     @Override
     public ResultEntity delete(SongList songList) {
         ResultEntity resultEntity=new ResultEntity();
 
-        if(songList.getId_SL()==null)
-        {
-            resultEntity.setInfo_error("<ERROR> id_SL is NULL");
+        try{
+            if(songList.getId_SL()==null)
+            {
+                resultEntity.setInfo_error("<ERROR> id_SL is NULL");
+                return resultEntity;
+            }
+
+            SongList songList_db=songListDao.queryByPK(songList);
+
+            if(songList_db==null)
+            {
+                resultEntity.setInfo_operation("歌单不存在");
+                return resultEntity;
+            }
+            //删除
+
+            //先删除中间映射
+            map_S_SL_DAO.deleteBySongListID(songList.getId_SL());
+            songListDao.deleteByPK(songList);
+            resultEntity.setState(true);
+            return resultEntity;
+        }catch (Exception e){
+            e.printStackTrace();
+            resultEntity.setState(false);
+            resultEntity.setInfo_error("删除歌单信息失败！");
             return resultEntity;
         }
 
-        SongList songList_db=songListDao.queryByPK(songList);
-
-        if(songList_db==null)
-        {
-            resultEntity.setInfo_operation("歌单不存在");
-            return resultEntity;
-        }
-        //删除
-
-        //先删除中间映射
-        map_S_SL_DAO.deleteBySongListID(songList.getId_SL());
-        songListDao.deleteByPK(songList);
-        resultEntity.setState(true);
-        return resultEntity;
     }
 }
